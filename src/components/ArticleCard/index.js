5fb7eae678a9a649b592a7e425f42aa385cbd694
jasmaa <jasonmaa3955@gmail.com>
2021-01-11 13:22:56 -0500

    Add article page. Fix image sizing on home.

--- src/components/ArticleCard/index.js
+++ src/components/ArticleCard/index.js
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
+
 import { getReadableTimeDifference } from 'utils';
 
 export default function ArticleCard({ dateNow, article }) {
   return (
     <div>
       <div className="flex flex-col h-full">
-        <img className="h-64 object-cover" src={article['header_image']} />
-        <p className="text-xl mt-5">{article['category']['label']}</p>
-        <p className="text-3xl mt-2">{article['title']}</p>
+        <Link to={`articles/${article['id']}`}>
+          <img className="w-full h-64 object-cover" src={article['header_image']} />
+        </Link>
+        <p className="text-xl font-semibold mt-5">{article['category']['label']}</p>
+        <Link to={`articles/${article['id']}`}>
+          <p className="text-3xl font-bold mt-2">{article['title']}</p>
+        </Link>
       </div>
       <p className="text-gray-500">{getReadableTimeDifference({ now: dateNow, start: new Date(article['date_edited']) })}</p>
       <hr className="mt-5" />
--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,7 +1,68 @@
+import { useState, useEffect } from 'react';
+import { Link, Redirect, useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+
+import client from 'config/client';
+
+
 export default function Article() {
+
+  const { id } = useParams();
+
+  const [article, setArticle] = useState({});
+  const [contents, setContents] = useState('');
+  const [isNotFound, setIsNotFound] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getArticle = async () => {
+      try {
+        const res = await client.get(`articles/${id}/`);
+        const contentsRes = await client.get(res.data['contents_file']);
+        setArticle(res.data);
+        setContents(contentsRes.data);
+        setIsLoading(false);
+      } catch (err) {
+        switch (err.response.status) {
+          case 404:
+            setIsNotFound(true);
+            break;
+          default:
+            // no-op
+            break;
+        }
+      }
+    }
+    getArticle();
+  }, []);
+
+  // Redirect to home if not found
+  if (isNotFound) {
+    return <Redirect to="/" />
+  }
+
+  if (isLoading) {
+    return <p>loading</p>
+  }
+
   return (
-    <div>
-      <h1>Article</h1>
+    <div className="px-5">
+      <div className="grid grid-cols-12 mt-5">
+        <div className="col-start-1 col-span-12 md:col-start-4 md:col-span-6">
+          <img className="w-full h-96 object-cover" src={article['header_image']} />
+          <h1 className="text-4xl font-bold mt-5">{article['category']['label']}</h1>
+          <h1 className="text-6xl font-extrabold mt-5">{article['title']}</h1>
+          <h1 className="text-3xl mt-5">
+            <span>By: </span>
+            <Link to={`authors/${article['author']['id']}`}>{article['author']['first_name']} {article['author']['last_name']}</Link>
+          </h1>
+          <h1 className="text-3xl mt-5">{new Date(article['date_created']).toUTCString()}</h1>
+          <hr className="my-10" />
+          <div className="prose prose-xl p-5">
+            <ReactMarkdown>{contents}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
